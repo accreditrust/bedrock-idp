6b749e17c4c13e8af95a7df3f9e605edd2afb411
Alex Lamar <alawi@vt.edu>
2016-06-06 15:56:44 -0400

    Remove the IDP's brIdentityService.

--- components/identity/identity.js
+++ components/identity/identity.js
@@ -10,15 +10,13 @@ define([
   './create-identity-controller',
   './passphrase-confirmation-directive',
   './identity-controller',
-  './identity-service',
   './identity-credentials-controller',
   './identity-selector-directive',
   './identity-settings-controller',
   './add-identity-modal-directive',
   './credential-verify-service'
 ], function(
-  angular, createIdentity, passphraseConfirmation, controller,
-  service, identityCredentials, identitySelector, identitySettingsController,
+  angular, createIdentity, passphraseConfirmation, controller, identityCredentials, identitySelector, identitySettingsController,
   modalAddIdentity, credentialVerifyService) {
 
 'use strict';
@@ -29,7 +27,6 @@ module.controller(createIdentity);
 module.controller(controller);
 module.controller(identityCredentials.controller);
 module.controller(identitySettingsController);
-module.service(service);
 module.service(credentialVerifyService);
 module.directive(passphraseConfirmation);
 module.directive(identitySelector);
